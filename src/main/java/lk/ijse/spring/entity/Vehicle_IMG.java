f82ec8b85313846d7cf69f5289fcfecb7155458c
pasindulakshankudaligama <kudaligamagepasindulakshan@gmail.com>
2022-07-06 02:21:12 +0530

    Vehicle_IMG entity completed

---
+++ src/main/java/lk/ijse/spring/entity/Vehicle_IMG.java
@@ -0,0 +1,30 @@
+package lk.ijse.spring.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Entity
+@ToString
+public class Vehicle_IMG {
+
+    @Id
+    private String img_Id;
+    @Id
+    private String registration_Number;
+    private String description;
+
+    @ManyToOne
+    @JoinColumn(name = "registration_Number",referencedColumnName = "registration_Number",insertable = false,updatable = false)
+    private Vehicle vehicle;
+
+}
