b1bc3854d9dd40dc4b5bb08f7de814c16fd5cd18
pasindulakshankudaligama <kudaligamagepasindulakshan@gmail.com>
2022-07-05 19:40:16 +0530

    dependencies & config classes added

---
+++ src/main/java/WebAppInitializer.java
@@ -0,0 +1,28 @@
+import lk.ijse.spring.cofig.WebAppConfig;
+import lk.ijse.spring.cofig.WebRootConfig;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class WebAppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[]{
+                WebRootConfig.class
+        };
+
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[]{
+                WebAppConfig.class
+        };
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{
+                "/"
+        };
+    }
+}
