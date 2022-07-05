428b1ed089b9c41ba77160bdf92eeadcbf26e3e1
pasindulakshankudaligama <kudaligamagepasindulakshan@gmail.com>
2022-07-05 21:58:54 +0530

    customer controller class added

---
+++ src/main/java/lk/ijse/spring/controller/CustomerController.java
@@ -0,0 +1,17 @@
+package lk.ijse.spring.controller;
+
+import lk.ijse.spring.service.CustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("customer")
+@CrossOrigin
+public class CustomerController {
+
+    @Autowired
+    CustomerService customerService;
+
+}
--- src/main/java/lk/ijse/spring/service/impl/CustomerServiceImpl.java
+++ src/main/java/lk/ijse/spring/service/impl/CustomerServiceImpl.java
@@ -4,7 +4,12 @@ import lk.ijse.spring.repo.CustomerRepo;
 import lk.ijse.spring.service.CustomerService;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+
+@Service
+@Transactional
 public class CustomerServiceImpl implements CustomerService {
     @Autowired
     private CustomerRepo repo;
