bc1621591d017f70e24bfe1111b987aab43b4cf7
pasindulakshankudaligama <kudaligamagepasindulakshan@gmail.com>
2022-07-05 21:47:07 +0530

    util class & advisor class added

---
+++ src/main/java/lk/ijse/spring/entity/Customer.java
@@ -0,0 +1,13 @@
+package lk.ijse.spring.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data // apply getters and setters
+@ToString
+public class Customer {
+}
