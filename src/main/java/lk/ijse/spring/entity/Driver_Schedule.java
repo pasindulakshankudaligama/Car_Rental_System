3c310e299d107bb6795ba27ce04e1d5d17e203b2
pasindulakshankudaligama <kudaligamagepasindulakshan@gmail.com>
2022-07-06 12:19:37 +0530

    Driver_Schedule entity completed

---
+++ src/main/java/lk/ijse/spring/entity/Driver_Schedule.java
@@ -0,0 +1,41 @@
+package lk.ijse.spring.entity;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Entity
+@ToString
+public class Driver_Schedule {
+    @Id
+    private String staff_Id;
+    @Id
+    private String registration_Number;
+   /* @Id
+    private String registration_Number;*/
+  /* @JsonFormat(pattern = "yyyy-MM-dd")*/
+    private LocalDate rent_Date;
+    private LocalDate return_Date;
+
+
+
+    @ManyToOne(cascade = {CascadeType.REFRESH,CascadeType.DETACH})
+    @JoinColumn(name = "staff_Id",referencedColumnName = "staff_Id",nullable = false)
+    private Staff staff;
+
+    @ManyToOne(cascade = {CascadeType.REFRESH,CascadeType.DETACH})
+    @JoinColumn(name = "registration_Number",referencedColumnName = "registration_Number",nullable = false)
+    private Vehicle vehicle;
+
+   /* @ManyToOne(cascade = {CascadeType.REFRESH,CascadeType.DETACH})
+    @JoinColumn(name = "registration_Number",referencedColumnName = "registration_Number",nullable = false)
+    private Vehicle vehicle;*/
+}
