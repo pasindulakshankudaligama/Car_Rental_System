db332512e4273cecd2c78768ebd05a486f4be4c3
pasindulakshankudaligama <kudaligamagepasindulakshan@gmail.com>
2022-07-06 11:31:57 +0530

    SlipIMG entity completed

---
+++ src/main/java/lk/ijse/spring/entity/Slip_IMG.java
@@ -0,0 +1,20 @@
+package lk.ijse.spring.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Entity
+@ToString
+public class Slip_IMG {
+    @Id
+    private String slip_IMG_Id;
+    private String description;
+}
