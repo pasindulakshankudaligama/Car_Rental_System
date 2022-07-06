0dffbd0aabda8b75057a494f30a27ade6e3d6b46
pasindulakshankudaligama <kudaligamagepasindulakshan@gmail.com>
2022-07-06 10:44:39 +0530

    licenseIMG entity completed

---
+++ src/main/java/lk/ijse/spring/entity/License_or_NIC_IMG.java
@@ -0,0 +1,21 @@
+package lk.ijse.spring.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Entity
+@ToString
+public class License_or_NIC_IMG {
+    @Id
+    private String img_Id;
+    private String description;
+
+}
--- src/main/java/lk/ijse/spring/entity/Vehicle.java
+++ src/main/java/lk/ijse/spring/entity/Vehicle.java
@@ -15,9 +15,25 @@ import javax.persistence.*;
 public class Vehicle {
     @Id
     private String registration_Number;
+    @Id
+    private String rate_Id;
+    @Id
+    private String vehicle_Type_Id;
+    private String colour;
+    private int no_Of_Passengers;
+    private String transmission_Type;
+    private String brand;
+    private String status;
+    private String fuel_Type;
+    private String running_Km;
+
 
 
     @ManyToOne(cascade = {CascadeType.REFRESH,CascadeType.DETACH})
-    @JoinColumn(name = "customerID",referencedColumnName = "id",nullable = false)
-    private Customer customer;
+    @JoinColumn(name = "rate_Id",referencedColumnName = "rate_Id",nullable = false)
+    private Rates rates;
+
+    @ManyToOne(cascade = {CascadeType.REFRESH,CascadeType.DETACH})
+    @JoinColumn(name = "vehicle_Type_Id",referencedColumnName = "vehicle_Type_Id",nullable = false)
+    private Vehicle_Type vehicle_type;
 }
