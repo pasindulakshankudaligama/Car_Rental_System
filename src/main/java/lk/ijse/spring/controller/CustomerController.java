cf9a9c2ffb9c24d2230b2b258acfed1cf5fc9a93
pasindulakshankudaligama <kudaligamagepasindulakshan@gmail.com>
2022-07-07 19:05:45 +0530

    customer controller added search customer method

--- src/main/java/lk/ijse/spring/controller/CustomerController.java
+++ src/main/java/lk/ijse/spring/controller/CustomerController.java
@@ -32,5 +32,11 @@ public class CustomerController {
         customerService.updateCustomer(customerDTO);
         return new ResponseUtil(200,"Updated",null);
     }
+    @GetMapping(path = "/{id}",produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseUtil searchCustomer(@PathVariable String id){
+        return new ResponseUtil(200,"OK",customerService.searchCustomer(id));
+    }
+
+
 
 }
