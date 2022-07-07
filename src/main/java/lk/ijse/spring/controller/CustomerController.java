99f8731fd747f2cb43ab4534e4a3200f7dccdcff
pasindulakshankudaligama <kudaligamagepasindulakshan@gmail.com>
2022-07-07 11:45:07 +0530

    customer controller added save customer method

--- src/main/java/lk/ijse/spring/controller/CustomerController.java
+++ src/main/java/lk/ijse/spring/controller/CustomerController.java
@@ -1,10 +1,12 @@
 package lk.ijse.spring.controller;
 
+import lk.ijse.spring.dto.CustomerDTO;
 import lk.ijse.spring.service.CustomerService;
+import lk.ijse.spring.util.ResponseUtil;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("customer")
@@ -13,5 +15,12 @@ public class CustomerController {
 
     @Autowired
     CustomerService customerService;
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseUtil saveCustomer(@ModelAttribute CustomerDTO customer){
+        customerService.saveCustomer(customer);
+        return new ResponseUtil(200,"Saved",null);
+    }
 
 }
