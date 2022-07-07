ddc5d4af5838e7af04533d81f18c09dd8a357305
pasindulakshankudaligama <kudaligamagepasindulakshan@gmail.com>
2022-07-07 11:11:26 +0530

    vehicle added rates id,vehicle type id column as join column

--- src/main/java/lk/ijse/spring/entity/Vehicle.java
+++ src/main/java/lk/ijse/spring/entity/Vehicle.java
@@ -15,10 +15,6 @@ import javax.persistence.*;
 public class Vehicle {
     @Id
     private String registration_Number;
-   // @Id
-    private String rate_Id;
-  //  @Id
-    private String vehicle_Type_Id;
     private String colour;
     private int no_Of_Passengers;
     private String transmission_Type;
@@ -27,13 +23,12 @@ public class Vehicle {
     private String fuel_Type;
     private String running_Km;
 
-
-/*
     @ManyToOne(cascade = {CascadeType.REFRESH,CascadeType.DETACH})
     @JoinColumn(name = "rate_Id",referencedColumnName = "rate_Id",nullable = false)
     private Rates rates;
 
     @ManyToOne(cascade = {CascadeType.REFRESH,CascadeType.DETACH})
     @JoinColumn(name = "vehicle_Type_Id",referencedColumnName = "vehicle_Type_Id",nullable = false)
-    private Vehicle_Type vehicle_type;*/
+    private Vehicle_Type vehicleType;
+
 }
