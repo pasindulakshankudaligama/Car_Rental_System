2c121c5ebf5009e6f5085ae45db59ca7258966bc
pasindulakshankudaligama <kudaligamagepasindulakshan@gmail.com>
2022-07-07 11:33:32 +0530

    customer serviceimpl added save customer method

--- src/main/java/lk/ijse/spring/service/impl/CustomerServiceImpl.java
+++ src/main/java/lk/ijse/spring/service/impl/CustomerServiceImpl.java
@@ -1,11 +1,15 @@
 package lk.ijse.spring.service.impl;
 
+import lk.ijse.spring.dto.CustomerDTO;
+import lk.ijse.spring.entity.Customer;
 import lk.ijse.spring.repo.CustomerRepo;
 import lk.ijse.spring.service.CustomerService;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 
 @Service
@@ -16,4 +20,33 @@ public class CustomerServiceImpl implements CustomerService {
 
     @Autowired
     private ModelMapper mapper;
+
+    @Override
+    public void saveCustomer(CustomerDTO dto) {
+        if(!repo.existsById(dto.getId())){
+            repo.save(mapper.map(dto, Customer.class));
+        }else{
+            throw new RuntimeException("Customer Already Exist..!");
+        }
+    }
+
+    @Override
+    public void deleteCustomer(String id) {
+
+    }
+
+    @Override
+    public void updateCustomer(CustomerDTO dto) {
+
+    }
+
+    @Override
+    public CustomerDTO searchCustomer(String id) {
+        return null;
+    }
+
+    @Override
+    public List<CustomerDTO> getAllCustomers() {
+        return null;
+    }
 }
