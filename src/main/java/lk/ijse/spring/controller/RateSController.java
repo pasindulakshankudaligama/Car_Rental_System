eee01751432fa142a4e69ff7e4633c61e210c898
pasindulakshankudaligama <kudaligamagepasindulakshan@gmail.com>
2022-07-09 00:34:09 +0530

    add save rates method rate controller

--- src/main/java/lk/ijse/spring/controller/RateSController.java
+++ src/main/java/lk/ijse/spring/controller/RateSController.java
@@ -1,10 +1,12 @@
 package lk.ijse.spring.controller;
 
+import lk.ijse.spring.dto.RatesDTO;
 import lk.ijse.spring.service.RateService;
+import lk.ijse.spring.util.ResponseUtil;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("rates")
@@ -14,5 +16,11 @@ public class RateSController {
     @Autowired
     RateService rateService;
 
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseUtil saveRates(@ModelAttribute RatesDTO ratesDTO){
+        rateService.saveRates(ratesDTO);
+        return new ResponseUtil(200,"Saved",null);
+    }
 
 }
