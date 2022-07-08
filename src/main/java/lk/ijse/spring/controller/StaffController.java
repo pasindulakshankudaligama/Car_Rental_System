355a2a93cde85e1190fe98307f10958f65857377
pasindulakshankudaligama <kudaligamagepasindulakshan@gmail.com>
2022-07-08 23:23:03 +0530

    add save staff method in staff controller

--- src/main/java/lk/ijse/spring/controller/StaffController.java
+++ src/main/java/lk/ijse/spring/controller/StaffController.java
@@ -1,10 +1,12 @@
 package lk.ijse.spring.controller;
 
+import lk.ijse.spring.dto.StaffDTO;
 import lk.ijse.spring.service.StaffService;
+import lk.ijse.spring.util.ResponseUtil;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("staff")
@@ -12,4 +14,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class StaffController {
     @Autowired
     StaffService staffService;
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseUtil saveStaff(@ModelAttribute StaffDTO staffDTO){
+        staffService.saveStaff(staffDTO);
+        return new ResponseUtil(200,"Saved",null);
+    }
 }
