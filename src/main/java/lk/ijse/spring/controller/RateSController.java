3360e05ee32de702dc8b9d7628afa5bc5b3692c7
pasindulakshankudaligama <kudaligamagepasindulakshan@gmail.com>
2022-07-09 00:38:36 +0530

    add delete rates method rate controller

--- src/main/java/lk/ijse/spring/controller/RateSController.java
+++ src/main/java/lk/ijse/spring/controller/RateSController.java
@@ -18,9 +18,13 @@ public class RateSController {
 
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseUtil saveRates(@ModelAttribute RatesDTO ratesDTO){
+    public ResponseUtil saveRate(@ModelAttribute RatesDTO ratesDTO){
         rateService.saveRates(ratesDTO);
         return new ResponseUtil(200,"Saved",null);
     }
-
+    @DeleteMapping(params={"id"},produces =MediaType.APPLICATION_JSON_VALUE)
+    public ResponseUtil deleteRate(@RequestParam String id){
+        rateService.deleteRates(id);
+        return new ResponseUtil(200,"Deleted",null);
+    }
 }
