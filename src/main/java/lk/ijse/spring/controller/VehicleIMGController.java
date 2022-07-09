a0c1d00748e3ae2d2367e674007c81cd159063ca
pasindulakshankudaligama <kudaligamagepasindulakshan@gmail.com>
2022-07-09 23:51:54 +0530

    add vehicle img cntroller to save vehicle img method

---
+++ src/main/java/lk/ijse/spring/controller/VehicleIMGController.java
@@ -0,0 +1,25 @@
+package lk.ijse.spring.controller;
+
+import lk.ijse.spring.dto.Vehicle_IMGDTO;
+import lk.ijse.spring.service.CustomerService;
+import lk.ijse.spring.service.VehicleIMGService;
+import lk.ijse.spring.util.ResponseUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("vehicleImg")
+@CrossOrigin
+public class VehicleIMGController {
+    @Autowired
+    VehicleIMGService vehicleIMGService;
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseUtil saveVehicleIMG(@ModelAttribute Vehicle_IMGDTO vehicle_imgdto){
+        vehicleIMGService.saveVehicleIMG(vehicle_imgdto);
+        return new ResponseUtil(200,"Saved",null);
+    }
+}
