0a525ff9623baff54113bb97c52f688babc0c8cb
pasindulakshankudaligama <kudaligamagepasindulakshan@gmail.com>
2022-07-10 00:00:46 +0530

    add slip img repo

---
+++ src/main/java/lk/ijse/spring/repo/SlipIMGRepo.java
@@ -0,0 +1,7 @@
+package lk.ijse.spring.repo;
+
+import lk.ijse.spring.entity.Slip_IMG;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SlipIMGRepo extends JpaRepository<Slip_IMG,String> {
+}
