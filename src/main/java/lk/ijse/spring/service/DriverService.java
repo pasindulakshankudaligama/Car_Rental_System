884188bbc4a9848348e8f57e85c79ddd7799e4a3
pasindulakshankudaligama <kudaligamagepasindulakshan@gmail.com>
2022-07-09 15:43:20 +0530

    add driver service

---
+++ src/main/java/lk/ijse/spring/service/DriverService.java
@@ -0,0 +1,17 @@
+package lk.ijse.spring.service;
+
+import lk.ijse.spring.dto.DriverDTO;
+
+import java.util.List;
+
+public interface DriverService {
+    void saveDriver(DriverDTO dto);
+
+    void deleteDriver(String id);
+
+    void updateDriver(DriverDTO dto);
+
+    DriverDTO searchDriver(String id);
+
+    List<DriverDTO> getAllDrivers();
+}
