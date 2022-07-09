4c62c744785df7c8227e486ce5b5bc0043f8fa81
pasindulakshankudaligama <kudaligamagepasindulakshan@gmail.com>
2022-07-09 15:32:43 +0530

    add driver entity & refactor

--- src/main/java/lk/ijse/spring/entity/Drive_Schedule_PK.java
+++ src/main/java/lk/ijse/spring/entity/Drive_Schedule_PK.java
@@ -4,14 +4,13 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Id;
 import java.io.Serializable;
 
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
 public class Drive_Schedule_PK implements Serializable {
-    private String staff_Id;
+    private String driver_Id;
     private String registration_Number;
     private String rent_Id;
 }
---
+++ src/main/java/lk/ijse/spring/entity/Driver.java
@@ -0,0 +1,25 @@
+package lk.ijse.spring.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Entity
+@ToString
+public class Driver {
+    @Id
+    private String driver_Id;
+    private String name;
+    private String address;
+    private String type;
+    private String mobile_Number;
+    private String email;
+    private String password;
+}
