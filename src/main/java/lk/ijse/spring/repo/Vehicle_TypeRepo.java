8ded9ea22907bc5125efa73c4a251a165dac654a
pasindulakshankudaligama <kudaligamagepasindulakshan@gmail.com>
2022-07-09 12:43:50 +0530

    add vehicle type repo

---
+++ src/main/java/lk/ijse/spring/repo/Vehicle_TypeRepo.java
@@ -0,0 +1,7 @@
+package lk.ijse.spring.repo;
+
+import lk.ijse.spring.entity.Vehicle_Type;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface Vehicle_TypeRepo extends JpaRepository<Vehicle_Type,String> {
+}
