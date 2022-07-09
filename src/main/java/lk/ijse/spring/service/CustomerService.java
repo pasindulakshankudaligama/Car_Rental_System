e893f39b3f07b0c25847e8d4d6961fb8eaa07930
pasindulakshankudaligama <kudaligamagepasindulakshan@gmail.com>
2022-07-09 18:58:21 +0530

    add vehicle service

--- src/main/java/lk/ijse/spring/service/CustomerService.java
+++ src/main/java/lk/ijse/spring/service/CustomerService.java
@@ -6,8 +6,12 @@ import java.util.List;
 
 public interface CustomerService {
     void saveCustomer(CustomerDTO dto);
+
     void deleteCustomer(String id);
+
     void updateCustomer(CustomerDTO dto);
+
     CustomerDTO searchCustomer(String id);
+
     List<CustomerDTO> getAllCustomers();
 }
