d44ff37efafecde58225fa26dd2f3af39b7c666d
pasindulakshankudaligama <kudaligamagepasindulakshan@gmail.com>
2022-07-09 23:41:19 +0530

    add vehicle img service impl

---
+++ src/main/java/lk/ijse/spring/service/impl/VehicleIMGServiceImpl.java
@@ -0,0 +1,28 @@
+package lk.ijse.spring.service.impl;
+
+import lk.ijse.spring.dto.Vehicle_IMGDTO;
+import lk.ijse.spring.service.VehicleIMGService;
+
+import java.util.List;
+
+public class VehicleIMGServiceImpl implements VehicleIMGService {
+    @Override
+    public void saveVehicleIMG(Vehicle_IMGDTO dto) {
+
+    }
+
+    @Override
+    public void deleteVehicleIMG(String id) {
+
+    }
+
+    @Override
+    public void updateVehicleIMG(Vehicle_IMGDTO dto) {
+
+    }
+
+    @Override
+    public List<Vehicle_IMGDTO> getAllVehicleIMGs() {
+        return null;
+    }
+}
