98d28e9e43b6c754e0e522d63b33a98d187eaa9d
pasindulakshankudaligama <kudaligamagepasindulakshan@gmail.com>
2022-07-09 12:51:16 +0530

    add vehicle type service impl

---
+++ src/main/java/lk/ijse/spring/service/impl/VehicleTypeImpl.java
@@ -0,0 +1,28 @@
+package lk.ijse.spring.service.impl;
+
+import lk.ijse.spring.dto.Vehicle_TypeDTO;
+import lk.ijse.spring.service.VehicleTypeService;
+
+import java.util.List;
+
+public class VehicleTypeImpl implements VehicleTypeService {
+    @Override
+    public void saveVehicleType(Vehicle_TypeDTO dto) {
+
+    }
+
+    @Override
+    public void deleteVehicleType(String id) {
+
+    }
+
+    @Override
+    public void updateVehicleType(Vehicle_TypeDTO dto) {
+
+    }
+
+    @Override
+    public List<Vehicle_TypeDTO> getAllVehicleType() {
+        return null;
+    }
+}
