6c869dbc14ff0d835a79c40009df80b4382cc2e0
pasindulakshankudaligama <kudaligamagepasindulakshan@gmail.com>
2022-07-10 01:37:21 +0530

    add rent service

---
+++ src/main/java/lk/ijse/spring/service/RentService.java
@@ -0,0 +1,17 @@
+package lk.ijse.spring.service;
+
+import lk.ijse.spring.dto.RentDTO;
+
+import java.util.List;
+
+public interface RentService {
+    void saveRent(RentDTO dto);
+
+    void deleteRent(String id);
+
+    void updateRent(RentDTO dto);
+
+    RentDTO searchRent(String id);
+
+    List<RentDTO> getAllRents();
+}
