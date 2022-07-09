964ea2ad70d7a4544b47b567c576677afcc209a4
pasindulakshankudaligama <kudaligamagepasindulakshan@gmail.com>
2022-07-09 15:44:35 +0530

    add driver service impl

---
+++ src/main/java/lk/ijse/spring/service/impl/DriverServiceImpl.java
@@ -0,0 +1,33 @@
+package lk.ijse.spring.service.impl;
+
+import lk.ijse.spring.dto.DriverDTO;
+import lk.ijse.spring.service.DriverService;
+
+import java.util.List;
+
+public class DriverServiceImpl implements DriverService {
+    @Override
+    public void saveDriver(DriverDTO dto) {
+
+    }
+
+    @Override
+    public void deleteDriver(String id) {
+
+    }
+
+    @Override
+    public void updateDriver(DriverDTO dto) {
+
+    }
+
+    @Override
+    public DriverDTO searchDriver(String id) {
+        return null;
+    }
+
+    @Override
+    public List<DriverDTO> getAllDrivers() {
+        return null;
+    }
+}
