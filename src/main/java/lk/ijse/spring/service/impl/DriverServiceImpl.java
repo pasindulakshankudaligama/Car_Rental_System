36edbbeab9536b4098407437272adca8f0a1895d
pasindulakshankudaligama <kudaligamagepasindulakshan@gmail.com>
2022-07-09 15:53:15 +0530

    add save driver method driver service impl

--- src/main/java/lk/ijse/spring/service/impl/DriverServiceImpl.java
+++ src/main/java/lk/ijse/spring/service/impl/DriverServiceImpl.java
@@ -1,14 +1,33 @@
 package lk.ijse.spring.service.impl;
 
 import lk.ijse.spring.dto.DriverDTO;
+import lk.ijse.spring.entity.Driver;
+import lk.ijse.spring.repo.DriverRepo;
 import lk.ijse.spring.service.DriverService;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.util.List;
 
+@Service
+@Transactional
 public class DriverServiceImpl implements DriverService {
+
+    @Autowired
+    private DriverRepo driverRepo;
+
+    @Autowired
+    private ModelMapper mapper;
+
     @Override
     public void saveDriver(DriverDTO dto) {
-
+        if (!driverRepo.existsById(dto.getDriver_Id())) {
+            driverRepo.save(mapper.map(dto, Driver.class));
+        }else{
+            throw new RuntimeException("Customer Already Exist..!");
+        }
     }
 
     @Override
