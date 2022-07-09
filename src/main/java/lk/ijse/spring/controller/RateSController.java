e25465b5a6dc64d84387762f16c679fb8b784e51
pasindulakshankudaligama <kudaligamagepasindulakshan@gmail.com>
2022-07-09 13:18:18 +0530

    add save vehicle type method in vehicle type controller

--- src/main/java/lk/ijse/spring/controller/RateSController.java
+++ src/main/java/lk/ijse/spring/controller/RateSController.java
@@ -1,6 +1,5 @@
 package lk.ijse.spring.controller;
 
-import lk.ijse.spring.dto.CustomerDTO;
 import lk.ijse.spring.dto.RatesDTO;
 import lk.ijse.spring.service.RateService;
 import lk.ijse.spring.util.ResponseUtil;
@@ -19,22 +18,25 @@ public class RateSController {
 
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseUtil saveRate(@ModelAttribute RatesDTO ratesDTO){
+    public ResponseUtil saveRate(@ModelAttribute RatesDTO ratesDTO) {
         rateService.saveRates(ratesDTO);
-        return new ResponseUtil(200,"Saved",null);
+        return new ResponseUtil(200, "Saved", null);
     }
-    @DeleteMapping(params={"id"},produces =MediaType.APPLICATION_JSON_VALUE)
-    public ResponseUtil deleteRate(@RequestParam String id){
+
+    @DeleteMapping(params = {"id"}, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseUtil deleteRate(@RequestParam String id) {
         rateService.deleteRates(id);
-        return new ResponseUtil(200,"Deleted",null);
+        return new ResponseUtil(200, "Deleted", null);
     }
+
     @PutMapping(produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseUtil updateCustomer(@RequestBody RatesDTO ratesDTO){
+    public ResponseUtil updateCustomer(@RequestBody RatesDTO ratesDTO) {
         rateService.updateRates(ratesDTO);
-        return new ResponseUtil(200,"Updated",null);
+        return new ResponseUtil(200, "Updated", null);
     }
+
     @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseUtil getAllCustomers(){
-        return new ResponseUtil(200,"OK",rateService.getAllRates());
+    public ResponseUtil getAllCustomers() {
+        return new ResponseUtil(200, "OK", rateService.getAllRates());
     }
 }
---
+++ src/main/java/lk/ijse/spring/controller/VehicleTypeController.java
@@ -0,0 +1,24 @@
+package lk.ijse.spring.controller;
+
+import lk.ijse.spring.dto.Vehicle_TypeDTO;
+import lk.ijse.spring.service.VehicleTypeService;
+import lk.ijse.spring.util.ResponseUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("vehicleType")
+@CrossOrigin
+public class VehicleTypeController {
+    @Autowired
+    VehicleTypeService vehicleTypeService;
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseUtil saveVehicleType(@ModelAttribute Vehicle_TypeDTO vehicle_typeDTO){
+        vehicleTypeService.saveVehicleType(vehicle_typeDTO);
+        return new ResponseUtil(200,"Saved",null);
+    }
+}
