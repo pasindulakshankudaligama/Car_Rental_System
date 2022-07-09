5e9398fd4f8075f575de3e0435738f23d028c248
pasindulakshankudaligama <kudaligamagepasindulakshan@gmail.com>
2022-07-10 01:08:49 +0530

    add update slip img method to slip img controller

--- src/main/java/lk/ijse/spring/controller/SlipIMGController.java
+++ src/main/java/lk/ijse/spring/controller/SlipIMGController.java
@@ -27,4 +27,9 @@ public class SlipIMGController {
         slipIMGService.deleteSlipIMG(id);
         return new ResponseUtil(200,"Deleted",null);
     }
+    @PutMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseUtil updateSlipImg(@RequestBody Slip_IMGDTO slip_imgdto){
+        slipIMGService.updateSlipIMG(slip_imgdto);
+        return new ResponseUtil(200,"Updated",null);
+    }
 }
