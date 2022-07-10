8dec3c2877a6e140516ffea9959a357ed6c2cf6b
pasindulakshankudaligama <kudaligamagepasindulakshan@gmail.com>
2022-07-10 15:31:21 +0530

    add rent controller to save rent method

--- src/main/java/lk/ijse/spring/service/impl/RentServiceImpl.java
+++ src/main/java/lk/ijse/spring/service/impl/RentServiceImpl.java
@@ -1,6 +1,8 @@
 package lk.ijse.spring.service.impl;
 
 import lk.ijse.spring.dto.RentDTO;
+import lk.ijse.spring.entity.Customer;
+import lk.ijse.spring.entity.Rent;
 import lk.ijse.spring.repo.RentRepo;
 import lk.ijse.spring.service.RentService;
 import org.modelmapper.ModelMapper;
@@ -21,7 +23,11 @@ public class RentServiceImpl implements RentService {
 
     @Override
     public void saveRent(RentDTO dto) {
-
+        if (!rentRepo.existsById(dto.getId())) {
+            rentRepo.save(mapper.map(dto, Rent.class));
+        } else {
+            throw new RuntimeException("Customer Already Exist..!");
+        }
     }
 
     @Override
