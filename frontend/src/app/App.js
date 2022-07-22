619bcc0ebe985a423468a388fdde8d68881d0794
pasindulakshankudaligama <kudaligamagepasindulakshan@gmail.com>
2022-07-22 23:37:11 +0530

    add dashboard page

--- frontend/src/app/App.js
+++ frontend/src/app/App.js
@@ -1,14 +1,12 @@
 import React from "react";
-import HomePage from "../pages/Home";
 import Login from "../pages/session/Login";
 import {Routes, Route} from "react-router-dom";
 import NotFound from "../pages/session/NotFound";
-import Posts from "../../src/pages/Posts";
+
 function App() {
+
   return (
       <Routes>
-        <Route exact path='/' element={<HomePage/>}/>
-        <Route exact path='posts' element={<Posts/>}/>
         <Route path='login' element={<Login/>}/>
         <Route path="*" element={<NotFound/>}/>
       </Routes>
