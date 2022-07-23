f3d8af3e7c76fb3afc1466150079fa49896a2c39
pasindulakshankudaligama <kudaligamagepasindulakshan@gmail.com>
2022-07-23 14:30:50 +0530

    user login js file updated

--- frontend/src/pages/session/Login/user/style.js
+++ frontend/src/pages/session/Login/user/style.js
@@ -1,37 +1,78 @@
 export const styleSheet = {
-    container: {
+
+    /* container:{
+        display:'flex',
+        // flexWrap:'wrap',
+        alignItems:'center',
+        justifyContent:'center',
+        // flexDirection:'column',
+        // backgroundColor:'#181818',
+        height:'100vh',
+        width:'100vw',
+        // color:'white'
+    }, */
+
+    login_cover: {
+        backgroundColor: 'red',
         display: 'flex',
         flexWrap: 'wrap',
+        // flexDirection: 'column',
+        height: '45%',
+        width: '30%',
+        paddingTop: '5px',
+        alignItems: 'center',
+        alignContent: 'center',
+        border: 'black solid',
+        borderRadius: '5px',
+        // color:'white'
+    },
+
+    title_container: {
+        display: 'flex',
         flexDirection: 'column',
-        height: '100vh',
+        // backgroundColor:'yellow',
+        height: '10vh',
         width: '100vw',
         alignItems: 'center',
         justifyContent: 'center'
     },
-    login__cover: {
+
+    form_container: {
         display: 'flex',
-        flexWrap: 'wrap',
-        flexDirection: 'column',
-        height: '70%',
-        width: '30%',
+        // backgroundColor:'green',
+        height: '20vh',
         alignItems: 'center',
         justifyContent: 'center',
-        border: 'black solid'
-    },
-    title__container: {
-        display: 'flex',
-        alignItems: 'center',
-        height: '20%',
-    },
-    form__container: {
-        display: 'flex',
+        width: '100vw',
         flexDirection: 'column',
-        height: '50%',
-        width: '100%',
-        alignItems: 'center',
-        justifyContent: 'space-around',
+        gap: '20px',
+        // color: 'white'
     },
-    btn__container: {
-        height: '20%',
+
+    btn_container: {
+        // backgroundColor:'blue',
+        height: '10vh',
+        width: '100vw',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+
+    btn: {
+        padding: '10px 50px !Important'
+    },
+
+
+    click: {
+        position: 'absolute',
+        top: '50%',
+        left: '50%',
+        transform: 'translate(-50%, -50%)',
+        width: 400,
+        bgcolor: 'background.paper',
+        border: '2px solid #000',
+        boxShadow: 24,
+        p: 4,
     }
+
 }
