cabcade3613aa5a148889c5c5ce05969a3c1b64b
pasindulakshankudaligama <kudaligamagepasindulakshan@gmail.com>
2022-07-26 23:02:45 +0530

    staff service post & get method done

---
+++ frontend/src/services/StaffService.js
@@ -0,0 +1,30 @@
+import axios from 'axios';
+import qs from 'qs';
+
+class StaffService {
+    postStaff = async (data) => {
+        const promise = new Promise((resolve, reject) => {
+            axios.post('staff', qs.stringify(data))
+                .then((res) => {
+                    return resolve(res)
+                }).catch((err) => {
+                return resolve(err)
+            })
+        });
+        return await promise;
+    }
+
+    fetchStaff = async (data) => {
+        const promise = new Promise((resolve, reject) => {
+            axios.get('staff')
+                .then((res) => {
+                    return resolve(res)
+                }).catch((err) => {
+                return resolve(err)
+            })
+        });
+        return await promise;
+    }
+}
+
+export default new StaffService();
