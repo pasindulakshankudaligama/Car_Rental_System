01329725059eb97cd77c34b510dfff7c9cb12b66
pasindulakshankudaligama <kudaligamagepasindulakshan@gmail.com>
2022-07-26 22:40:27 +0530

    customer service post & get method done

---
+++ frontend/src/services/CustomerService.js
@@ -0,0 +1,30 @@
+import axios from 'axios';
+import qs from 'qs';
+
+class CustomerService {
+    postCustomer = async (data) => {
+        const promise = new Promise((resolve, reject) => {
+            axios.post('customer', qs.stringify(data))
+                .then((res) => {
+                    return resolve(res)
+                }).catch((err) => {
+                return resolve(err)
+            })
+        });
+        return await promise;
+    }
+
+    fetchCustomer = async (data) => {
+        const promise = new Promise((resolve, reject) => {
+            axios.get('customer')
+                .then((res) => {
+                    return resolve(res)
+                }).catch((err) => {
+                return resolve(err)
+            })
+        });
+        return await promise;
+    }
+}
+
+export default new CustomerService();
