5b792746a6aaf69e3e297997aa205e7c1f96429e
pasindulakshankudaligama <kudaligamagepasindulakshan@gmail.com>
2022-07-27 23:59:34 +0530

    vehicle crud completed

--- frontend/src/pages/AdminDashboard/Vehicle Manage/index.jsx
+++ frontend/src/pages/AdminDashboard/Vehicle Manage/index.jsx
@@ -1,23 +1,15 @@
-import {
-    Dialog,
-    DialogContent,
-    DialogTitle,
-    Grid,
-    IconButton,
-    Typography,
-} from "@mui/material";
-import React, { Component } from "react";
-import { Outlet } from "react-router-dom";
+import {Dialog, DialogContent, DialogTitle, Grid, IconButton, Typography,} from "@mui/material";
+import React, {Component} from "react";
 import Navbar from "../../../components/common/NavBar/Admin";
 import Sidebar from "../../../components/common/Sidebar";
 import CommonButton from "../../../components/common/Button";
 import CommonDataTable from "../../../components/common/Table";
 import AddIcon from "@mui/icons-material/Add";
-import { withStyles } from "@mui/styles";
-import { styleSheet } from "./style";
+import {withStyles} from "@mui/styles";
+import {styleSheet} from "./style";
 import CloseIcon from "@mui/icons-material/Close";
-import LoginUser from "../../session/Login/user";
 import AddNewVehicle from "../../../components/AddVehicle";
+
 class VehicleManage extends Component {
     constructor(props) {
         super(props);
@@ -36,32 +28,32 @@ class VehicleManage extends Component {
                 {
                     field: "registrationNumber",
                     headerName: "Reg Num",
-                    width: 90,
+                    width: 150,
                 },
 
                 {
                     field: "brand",
                     headerName: "Brand",
-                    width: 80,
+                    width: 150,
                 },
 
                 {
                     field: "color",
                     headerName: "color",
-                    width: 100,
+                    width: 125,
                     sortable: false,
                 },
 
                 {
                     field: "fuelType",
-                    headerName: "fuleType",
-                    width: 100,
+                    headerName: "Fule Type",
+                    width: 150,
                 },
 
                 {
                     field: "noOfPassengers",
                     headerName: "Num. Passengers",
-                    width: 100,
+                    width: 150,
                 },
 
                 {
@@ -72,26 +64,26 @@ class VehicleManage extends Component {
 
                 {
                     field: "transmissionType",
-                    headerName: "Transmition Type",
-                    width: 100,
+                    headerName: "Transmission Type",
+                    width: 150,
                 },
 
                 {
-                    field: "userId",
+                    field: "rates",
                     headerName: "Rates",
-                    width: 100,
+                    width: 150,
                 },
 
                 {
-                    field: "userId",
-                    headerName: "type",
-                    width: 100,
+                    field: "type",
+                    headerName: "Type",
+                    width: 150,
                 },
 
                 {
                     field: "status",
                     headerName: "Status",
-                    width: 100,
+                    width: 150,
                 },
 
                 {
@@ -105,74 +97,7 @@ class VehicleManage extends Component {
 
     async loadData() {
         // let resp = await PostService.fetchPosts();
-        const data = [
-            {
-                userId: 156984,
-                id: "Reg00-001",
-                title:
-                    "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
-            },
-            {
-                userId: 1,
-                id: 2,
-                title: "qui est esse",
-            },
-            {
-                userId: 1,
-                id: 3,
-                title: "ea molestias quasi exercitationem repellat qui ipsa sit aut",
-            },
-            {
-                userId: 1,
-                id: 4,
-                title: "eum et est occaecati",
-            },
-            {
-                userId: 1,
-                id: 5,
-                title: "nesciunt quas odio",
-            },
-            {
-                userId: 1,
-                id: 6,
-                title: "dolorem eum magni eos aperiam quia",
-            },
-            {
-                userId: 1,
-                id: 7,
-                title: "magnam facilis autem",
-            },
-            {
-                userId: 1,
-                id: 8,
-                title: "dolorem dolore est ipsam",
-            },
-            {
-                userId: 1,
-                id: 9,
-                title: "nesciunt iure omnis dolorem tempora et accusantium",
-            },
-            {
-                userId: 1,
-                id: 10,
-                title: "optio molestias id quia eum",
-            },
-            {
-                userId: 2,
-                id: 11,
-                title: "et ea vero quia laudantium autem",
-            },
-            {
-                userId: 2,
-                id: 12,
-                title: "in quibusdam tempore odit est dolorem",
-            },
-            {
-                userId: 2,
-                id: 13,
-                title: "dolorum ut in voluptas mollitia et saepe quo animi",
-            },
-        ];
+        const data = [];
         this.setState({
             loaded: true,
             data: data,
@@ -187,14 +112,14 @@ class VehicleManage extends Component {
     }
 
     render() {
-        const { classes } = this.props;
+        const {classes} = this.props;
         return (
             <Grid container direction={"row"} columns="12">
                 <Grid item xs={"auto"}>
-                    <Sidebar />
+                    <Sidebar/>
                 </Grid>
                 <Grid item xs className="">
-                    <Navbar />
+                    <Navbar/>
                     <Grid container item xs={"auto"} className="flex p-5 gap-5">
                         <Grid
                             container
@@ -206,18 +131,18 @@ class VehicleManage extends Component {
                             <CommonButton
                                 variant="outlined"
                                 label="Add Vehicle"
-                                onClick={() => this.setState({ popup: true })}
-                                startIcon={<AddIcon />}
+                                onClick={() => this.setState({popup: true})}
+                                startIcon={<AddIcon/>}
                             />
                             <CommonButton
                                 variant="outlined"
                                 label="Add Vehicle Rates"
-                                startIcon={<AddIcon />}
+                                startIcon={<AddIcon/>}
                             />
                             <CommonButton
                                 variant="outlined"
                                 label="Add Vehicle Types"
-                                startIcon={<AddIcon />}
+                                startIcon={<AddIcon/>}
                             />
                         </Grid>
                         <Grid
@@ -226,7 +151,7 @@ class VehicleManage extends Component {
                             xs={12}
                             gap="5px"
                             className="rounded-lg p-5 shadow-[0_3px_10px_rgb(0,0,0,0.2)]"
-                            style={{ height: "700px" }}
+                            style={{height: "700px"}}
                         >
                             <CommonDataTable
                                 columns={this.state.columns}
@@ -241,21 +166,21 @@ class VehicleManage extends Component {
                 <Dialog
                     open={this.state.popup}
                     maxWidth="md"
-                    classes={{ paper: classes.dialogWraper }}
+                    classes={{paper: classes.dialogWraper}}
                 >
-                    <DialogTitle style={{ paddingRight: "0px" }}>
-                        <div style={{ display: "flex" }}>
+                    <DialogTitle style={{paddingRight: "0px"}}>
+                        <div style={{display: "flex"}}>
                             <Typography
                                 variant="h4"
                                 component="div"
                                 className="font-bold flex-grow"
-                                style={{ flexGrow: 1 }}
+                                style={{flexGrow: 1}}
                             >
                                 Add New Vehicle
                             </Typography>
 
-                            <IconButton onClick={() => this.setState({ popup: false })}>
-                                <CloseIcon />
+                            <IconButton onClick={() => this.setState({popup: false})}>
+                                <CloseIcon/>
                             </IconButton>
                         </div>
                     </DialogTitle>
@@ -267,4 +192,5 @@ class VehicleManage extends Component {
         );
     }
 }
+
 export default withStyles(styleSheet)(VehicleManage);
