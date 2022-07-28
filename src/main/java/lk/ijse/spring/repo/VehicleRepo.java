df7b331d64f920f5942c3e9f3494f912f7b7daba
pasindulakshankudaligama <kudaligamagepasindulakshan@gmail.com>
2022-07-28 21:43:23 +0530

    vehicle repo updated

--- src/main/java/lk/ijse/spring/repo/VehicleRepo.java
+++ src/main/java/lk/ijse/spring/repo/VehicleRepo.java
@@ -3,5 +3,11 @@ package lk.ijse.spring.repo;
 import lk.ijse.spring.entity.Vehicle;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface VehicleRepo extends JpaRepository<Vehicle,String> {
+    List<Vehicle> searchVehiclesByStatus(String status);
+
+    long countVehiclesByStatus(String status);
+
 }
