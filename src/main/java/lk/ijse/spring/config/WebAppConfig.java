1aef602e780d36fb3a7245ef1304bc4a639247fa
pasindulakshankudaligama <kudaligamagepasindulakshan@gmail.com>
2022-08-02 19:01:21 +0530

    implements web mvc & added addResourehandler method

--- src/main/java/lk/ijse/spring/config/WebAppConfig.java
+++ src/main/java/lk/ijse/spring/config/WebAppConfig.java
@@ -8,11 +8,13 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.multipart.MultipartResolver;
 import org.springframework.web.multipart.support.StandardServletMultipartResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @EnableWebMvc
 @Configuration
 @ComponentScan(basePackages = "lk.ijse.spring")
-public class WebAppConfig {
+public class WebAppConfig implements WebMvcConfigurer {
     @Bean
     public ModelMapper modelMapper(){
         return new ModelMapper();
@@ -26,5 +28,9 @@ public class WebAppConfig {
     @Bean
     public MultipartResolver multipartResolver(){
         return new StandardServletMultipartResolver();
+    }
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/uploads/**").addResourceLocations("/uploads/");
     }
 }
