34953a49bc8bde0476a170b20aa864b45d442feb
pasindulakshankudaligama <kudaligamagepasindulakshan@gmail.com>
2022-08-04 18:56:39 +0530

    license or nic img pk class added

---
+++ src/main/java/lk/ijse/spring/entity/LicenseOrNICIMG_PK.java
@@ -0,0 +1,8 @@
+package lk.ijse.spring.entity;
+
+import java.io.Serializable;
+
+public class LicenseOrNICIMG_PK implements Serializable {
+    private String img_Id;
+    private String customerID;
+}
