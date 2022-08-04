c1927bdcf0b44693d6e998010e1c2a5f8153f8ba
pasindulakshankudaligama <kudaligamagepasindulakshan@gmail.com>
2022-08-04 19:23:50 +0530

    customer service impl class save customer img method done

--- src/main/java/lk/ijse/spring/service/impl/CustomerServiceImpl.java
+++ src/main/java/lk/ijse/spring/service/impl/CustomerServiceImpl.java
@@ -1,6 +1,9 @@
 package lk.ijse.spring.service.impl;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lk.ijse.spring.dto.CustomerDTO;
+import lk.ijse.spring.dto.License_or_NIC_IMGDTO;
 import lk.ijse.spring.entity.Customer;
 import lk.ijse.spring.repo.CustomerRepo;
 import lk.ijse.spring.service.CustomerService;
@@ -9,7 +12,12 @@ import org.modelmapper.TypeToken;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
 import java.util.List;
 
 
@@ -21,6 +29,9 @@ public class CustomerServiceImpl implements CustomerService {
 
     @Autowired
     private ModelMapper mapper;
+
+    @Autowired
+    private ObjectMapper objectMapper;
 
     @Override
     public void saveCustomer(CustomerDTO dto) {
@@ -62,7 +73,7 @@ public class CustomerServiceImpl implements CustomerService {
 
     @Override
     public List<CustomerDTO> getAllCustomers() {
-        return mapper.map(repo.findAll(),new TypeToken<List<CustomerDTO>>(){
+        return mapper.map(repo.findAll(), new TypeToken<List<CustomerDTO>>() {
         }.getType());
     }
 
@@ -70,4 +81,38 @@ public class CustomerServiceImpl implements CustomerService {
     public long countCustomers() {
         return repo.count();
     }
+
+    @Override
+    public void saveCustomerWithImg(String customer, MultipartFile file) {
+        CustomerDTO dto = null;
+        String path = null;
+        try {
+            dto = objectMapper.readValue(customer, CustomerDTO.class);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+        if (!repo.existsById(dto.getId())) {
+            try {
+                String projectPath = new File(this.getClass().getProtectionDomain().getCodeSource().getLocation().toURI()).getParentFile().getParentFile().getAbsolutePath();
+                File uploadDir = new File(projectPath + "/uploads");
+                uploadDir.mkdir();
+                file.transferTo(new File(uploadDir.getAbsolutePath() + "/" + dto.getId() + "_" + file.getOriginalFilename()));
+                path = "uploads/" + dto.getId() + "_" + file.getOriginalFilename();
+            } catch (URISyntaxException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            License_or_NIC_IMGDTO imgDTO = new License_or_NIC_IMGDTO();
+            imgDTO.setPath(path);
+            ArrayList<License_or_NIC_IMGDTO> arrayList = new ArrayList<>();
+            arrayList.add(imgDTO);
+            System.out.println(imgDTO.getPath());
+            dto.setImg(arrayList);
+            repo.save(mapper.map(dto, Customer.class));
+        } else {
+            throw new RuntimeException("Customer Already Exist");
+        }
+    }
 }
+
